d81b2d3724ae2b688c4eb78f716293f50ca4c812
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-10-24 11:20:33 +0530

    Update MovieService.java

--- backend/backend/src/main/java/com/dk/backend/service/MovieService.java
+++ backend/backend/src/main/java/com/dk/backend/service/MovieService.java
@@ -1,7 +1,7 @@
 package com.dk.backend.service;
 
-
 import com.dk.backend.entity.Movie;
+import com.dk.backend.exception.ResourceNotFoundException;
 import com.dk.backend.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +18,25 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    public List<Movie> searchMovies(String title) {
+        return movieRepository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public List<Movie> getMoviesByGenre(String genre) {
+        return movieRepository.findByGenreIgnoreCase(genre);
+    }
+
     public Movie addMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public Movie updateMovie(Long id, Movie updatedMovie) {
+        Movie movie = movieRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Movie not found"));
+        movie.setTitle(updatedMovie.getTitle());
+        movie.setGenre(updatedMovie.getGenre());
+        movie.setDescription(updatedMovie.getDescription());
+        movie.setReleaseDate(updatedMovie.getReleaseDate());
         return movieRepository.save(movie);
     }
 

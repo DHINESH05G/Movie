9e53c94eaf12addbf15461033d71341ebda82ee5
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-10-30 10:23:42 +0530

    Update VideoService.java

--- backend/backend/src/main/java/com/dk/backend/service/VideoService.java
+++ backend/backend/src/main/java/com/dk/backend/service/VideoService.java
@@ -1,5 +1,14 @@
 package com.dk.backend.service;
 
+import jakarta.annotation.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+
 @Service
 public class VideoService {
 
@@ -9,7 +18,7 @@ public class VideoService {
         try {
             File videoFile = new File(VIDEO_DIR + "/" + videoName);
             Path videoPath = videoFile.toPath();
-            return new UrlResource(videoPath.toUri());
+            return (Resource) new UrlResource(videoPath.toUri());
         } catch (MalformedURLException e) {
             throw new RuntimeException("Error retrieving video", e);
         }

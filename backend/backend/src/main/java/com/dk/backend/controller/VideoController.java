ba7132a28b66d8b534aff3ea026d1d72cfe9815e
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-10-30 10:08:14 +0530

    Update VideoController.java

--- backend/backend/src/main/java/com/dk/backend/controller/VideoController.java
+++ backend/backend/src/main/java/com/dk/backend/controller/VideoController.java
@@ -1,12 +1,17 @@
 package com.dk.backend.controller;
 
-import jakarta.annotation.Resource;
+import com.dk.backend.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/videos")
@@ -16,11 +21,11 @@ public class VideoController {
     private VideoService videoService;
 
     @GetMapping("/{videoName}")
-    public ResponseEntity<Resource> streamVideo(@PathVariable String videoName, @RequestHeader HttpHeaders headers) {
-        Resource video = videoService.getVideoResource(videoName, headers);
+    public ResponseEntity<Resource> streamVideo(@PathVariable String videoName, @RequestHeader HttpHeaders headers) throws IOException {
+        Resource video = (Resource) videoService.getVideoResource(videoName, headers);
+        Path videoPath = Paths.get(video.getURI());
         return ResponseEntity.ok()
-                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .contentType(MediaTypeFactory.getMediaType((Resource) videoPath).orElse(MediaType.APPLICATION_OCTET_STREAM))
                 .body(video);
     }
 }
-

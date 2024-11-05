9238c0dbf52446e705eb1f9ec957f4ba7f5c2374
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-05 17:57:09 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/auth/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { MovieDetailComponent } from './components/movies/movie-detail/movie-detail.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'movies/:id', component: MovieDetailComponent },
   { path: '**', redirectTo: '' }  // Catch-all route
 ];
 

fe90741a476058ee588d73a5dfb267db1877a516
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-06 10:41:42 +0530

    Update login.component.ts

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit(): void {
+    this.authService.login({ username: this.username, password: this.password }).subscribe(
+      response => {
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/']);
+      },
+      error => {
+        alert('Invalid credentials');
+      }
+    );
+  }
 }

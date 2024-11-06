56c887dd60db63106c726f63a9af4b9af4842c50
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-06 15:58:18 +0530

    Update profile.component.ts

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: any = null;
 
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.profileService.getUserProfile().subscribe({
+      next: data => this.user = data,
+      error: err => console.log('Error fetching profile', err)
+    });
+  }
+
+  onSubmit(): void {
+    this.profileService.updateUserProfile(this.user).subscribe({
+      next: response => alert('Profile updated successfully!'),
+      error: err => console.log('Error updating profile', err)
+    });
+  }
 }

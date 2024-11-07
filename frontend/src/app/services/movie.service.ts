f22767bcc02723e13261a5c7505bd97fc90b9960
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-07 10:01:16 +0530

    commit

---
+++ frontend/src/app/services/movie.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private baseUrl = 'http://localhost:8080/api/movies';
+
+  constructor(private http: HttpClient) { }
+
+  getMovies(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+  getMovieById(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+  
+}

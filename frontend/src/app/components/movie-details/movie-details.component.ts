dc4790d8c1887d2101aa4d18569c79dedbd98678
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-07 11:02:01 +0530

    created movie component

---
+++ frontend/src/app/components/movie-details/movie-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.css']
+})
+export class MovieDetailsComponent implements OnInit {
+  movie: any = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit(): void {
+    const movieId = this.route.snapshot.paramMap.get('id');
+    if (movieId) {
+      this.movieService.getMovieById(movieId).subscribe(
+        data => this.movie = data,
+        error => console.error('Error fetching movie details:', error)
+      );
+    }
+  }
+
+  playMovie(): void {
+    alert("Playing movie: " + this.movie.title);
+    // Here you would handle the video player logic or route to a player page
+  }
+}

482f53952524abce96aa218b4777589d85a60119
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-12 15:38:17 +0530

    update video-player component

--- frontend/src/app/components/video-player/video-player.component.ts
+++ frontend/src/app/components/video-player/video-player.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.css']
 })
-export class VideoPlayerComponent {
+export class VideoPlayerComponent implements OnInit {
+  @Input() movieId: string = '';
+  videoUrl: SafeUrl | undefined;
 
+  constructor(private movieService: MovieService, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.loadVideo();
+  }
+
+  loadVideo(): void {
+    this.movieService['getMovieStreamUrl'](this.movieId).subscribe(
+      (      url: string) => this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url),
+      (      error: any) => console.error('Error loading video:', error)
+    );
+  }
 }

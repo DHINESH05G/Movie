44cf23e92b0520a22eaf4d45cc82faf9e65f33ed
DKUMA110 <dhineshkumar7639733930@gmail.com>
2024-11-12 10:36:07 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuard } from './guards/auth.guard';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { WatchlistComponent } from './components/watchlist/watchlist.component';
 
 const routes: Routes = [
   { path: '**', redirectTo: '' },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: 'movies/:id', component: MovieDetailsComponent },
+  { path: 'watchlist', component: WatchlistComponent },
+
 
 ];
 
